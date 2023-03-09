9822cef90d945be3000ff00f662e87ccbb8904c2
quarkusio <no-reply@quarkus.io>
2023-03-09 17:48:58 +0000

    Initial commit

---
+++ src/main/kotlin/com/dmartinc/StartWebSocket.kt
@@ -0,0 +1,31 @@
+package com.dmartinc
+
+import javax.enterprise.context.ApplicationScoped
+import javax.websocket.*
+import javax.websocket.server.PathParam
+import javax.websocket.server.ServerEndpoint
+
+@ApplicationScoped
+@ServerEndpoint("/start-websocket/{name}")
+class StartWebSocket {
+
+    @OnOpen
+    fun onOpen(session: Session?, @PathParam("name") name: String) {
+        println("onOpen> $name")
+    }
+
+    @OnClose
+    fun onClose(session: Session?, @PathParam("name") name: String) {
+        println("onClose> $name")
+    }
+
+    @OnError
+    fun onError(session: Session?, @PathParam("name") name: String, throwable: Throwable) {
+        println("onError> $name: $throwable")
+    }
+
+    @OnMessage
+    fun onMessage(message: String, @PathParam("name") name: String) {
+        println("onMessage> $name: $message")
+    }
+}
